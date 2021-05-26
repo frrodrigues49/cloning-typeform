6fb07bed17aae68f53465770e71d1bbb416a8a2b
Fagner <frodriguesbetim@gmail.com>
2021-05-26 20:04:30 -0300

    Initial config

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import FirstAndSecond from './components/FirstAndSecond';
+import Header from './components/Header';
+import { Main, Section, Sticky } from './styles';
+import GlobalStyles from './styles/global'
+
+function App() {
+  React.useEffect(() => {
+    window.scrollTo({ top: 0 })
+  },[])
+
+  return (
+   <>
+    <GlobalStyles />
+
+    <Header />
+
+    <Main style={{ height: '1610vh' }}>
+      <Section style={{ height: '72.7%' }}>
+        <FirstAndSecond />      
+      </Section>
+      <Section style={{ height: '9.7%' }}>
+        <Sticky className="third" />      
+      </Section>
+      <Section style={{ height: '10.1%' }}>
+        <Sticky className="fourth" />      
+      </Section>     
+    </Main>
+   </>
+  );
+}
+
+export default App;
